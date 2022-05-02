f4417e5ecaf63f93527dce89c40190bb38eeeccc
Max Zatsarnyi <zatsarnyi.max@gmail.com>
2022-05-02 11:28:16 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 import { Routes } from './routes/index';
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div>
+        <Header />
         <Routes />
+        <Footer />
       </div>
     </Suspense>
   );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Home } from '../components/Home/Home';
 
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 };
 export default Homepage;
